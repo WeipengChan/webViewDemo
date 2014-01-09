df55391df0bc7d5ec54d3cdac1d0ca5725edb476
Robin_Weipeng_Chan <280296084@qq.com>
2014-01-09 15:33:06 +0800

    。。

---
+++ webViewDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  webViewDemo
+//
+//  Created by YunInfo on 14-1-8.
+//  Copyright (c) 2014年 Yuninfo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+@end
---
+++ webViewDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  webViewDemo
+//
+//  Created by YunInfo on 14-1-8.
+//  Copyright (c) 2014年 Yuninfo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
